7613a65c6fc37e6ae513a98ae917c6b44d5599ba
Irasaac <irasaac@gmail.com>
2018-02-24 16:36:31 +0200

    Update db.php

--- admin/db.php
+++ admin/db.php
@@ -1,6 +1,6 @@
 <?php  
 
-$db = new mysqli("localhost", "root", "phpcoder" , "sawasawa");
+$db = new mysqli("localhost", "root", "clement123" , "sawasawa");
 	
 	if($db->connect_errno){
 		die('Sorry we have some problem with the Database!');
